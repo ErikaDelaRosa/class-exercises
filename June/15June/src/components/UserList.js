0d2690e31cf2001089efd6a78e1ed2f46734cc5d
ErikaDelaRosa <erika-delarosa@hotmail.com>
2020-06-15 15:02:52 +0200

    daily exercises

---
+++ June/15June/src/components/UserList.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const UserList = (props) => {
+  const toDos = props.list.map((item, i) => (
+    <li key={i}>
+      {item}{" "}
+      <i
+        onClick={() => {
+          console.log(item);
+          props.del(item);
+        }}
+      >
+        🗑
+      </i>
+    </li>
+  ));
+  return <ul>{toDos}</ul>;
+};
+
+export default UserList;
+
+//individual key is mandatory
